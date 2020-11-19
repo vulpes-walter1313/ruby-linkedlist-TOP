341c73b9e57f38c84ff82f9aa020d29409249a1e
Walter Fritsche <wfritsche95@protonmail.com>
2020-11-19 16:19:20 -0600

    init commit

---
+++ LinkedList.rb
@@ -0,0 +1,38 @@
+class LinkedList
+  def initialize
+    @head = nil
+    @tail = nil
+  end
+
+  def append(value)
+  end
+  
+  def prepend(value)
+  end
+  
+  def size
+  end
+
+  def head
+  end
+  
+  def tail
+  end
+  
+  def at(index)
+  end
+
+  def pop
+  end
+  
+  def contains?(value)
+  end
+  
+  def find(value)
+  end
+  
+  def to_s
+  end
+
+
+end
---
+++ Node.rb
@@ -0,0 +1,6 @@
+class Node
+  def initialize(value)
+    @data = value
+    @next = nil
+  end
+end
